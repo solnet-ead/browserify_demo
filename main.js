7bbf7addfd018ad565971c8af3a4049d9f1b1b3b
Stephen Z <stephen.zhang@solnet.co.nz>
2015-11-10 12:53:11 +1300

    make something to bundle

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var unique = require('uniq');
+var url = require('url');
 
 var data = [1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 7];
 
 console.log(unique(data));
+
+document.body.innerHTML = JSON.stringify(url.parse(window.location.href));
